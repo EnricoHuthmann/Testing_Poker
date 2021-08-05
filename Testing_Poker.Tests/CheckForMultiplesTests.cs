510d7346fc0e7b2b30742e89f38ea32bd10674bb
Noxfean <Noxfean@gmail.com>
2021-08-05 12:18:31 +0200

    added base test method for pair check

---
+++ Testing_Poker.Tests/CheckForMultiplesTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Testing_Poker.Tests
+{
+    public class CheckForMultiplesTests
+    {
+        public CheckForMultiplesTests()
+        {
+        }
+
+        [Theory]
+        [InlineData(new object[] { new string[] { "H2" }, false })]
+        public void CheckForTwoPair_ReturnsFalse_WhenNoPairIsGiven(string[] givenCards, bool expectedResult)
+        {
+            
+        }
+    }
+}
